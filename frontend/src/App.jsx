ad5fc01d3779b41f54f59c1cf75818228da96622
Ryan Adiyantara <adiyantararyan@gmail.com>
2024-11-03 10:13:08 +0700

    Created route paths, ready for adjustments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,42 @@
-import { Button } from "@chakra-ui/react";
+import { Box, useColorModeValue } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+
+import Login from "./pages/login";
+import ForgotPassword from "./pages/forgotpassword";
+import Dashboard from "./pages/dashboard";
+import EmployeeDirectory from "./pages/employeedirectory";
+// ESS
+import ChangePassword from "./pages/ess.changepassword";
+import LeaveApplication from "./pages/ess.leaveapplication";
+// HR
+import MasterDepartment from "./pages/hr.masterdepartment";
+import MasterPosition from "./pages/hr.masterposition";
+import ManageEvent from "./pages/hr.manageevent";
+import ManageEmployee from "./pages/hr.manageemployee";
+import EmployeeTerminated from "./pages/hr.employeeterminated";
+import AttendanceSummary from "./pages/hr.attendancesummary";
+import LeaveApproval from "./pages/hr.leaveapproval";
 
 function App() {
   return (
     <>
-      <Button>Hello</Button>
+      <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/login/forgotpassword" element={<ForgotPassword />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/employeedirectory" element={<EmployeeDirectory />} />
+          <Route path="/ess/changepassword" element={<ChangePassword />} />
+          <Route path="/ess/leaveapplication" element={<LeaveApplication />} />
+          <Route path="/hr/masterdepartment" element={<MasterDepartment />} />
+          <Route path="/hr/masterposition" element={<MasterPosition />} />
+          <Route path="/hr/manageevent" element={<ManageEvent />} />
+          <Route path="/hr/manageemployee" element={<ManageEmployee />} />
+          <Route path="/hr/employeeterminated" element={<EmployeeTerminated />} />
+          <Route path="/hr/attendancesummary" element={<AttendanceSummary />} />
+          <Route path="/hr/leaveapproval" element={<LeaveApproval />} />
+        </Routes>
+      </Box>
     </>
   );
 }
---
+++ frontend/src/pages/hr.masterposition.jsx
@@ -0,0 +1,62 @@
+import { Button, Text, VStack } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+const MasterPosition = () => {
+  return (
+    <>
+      <VStack spacing={2} alignItems={"center"}>
+        <Text
+          fontSize={{ base: "22", sm: "28" }}
+          fontWeight={"bold"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+        >
+          Master Position
+        </Text>
+        <Link to={"/login"}>
+          <Button>Login</Button>
+        </Link>
+        <Link to={"/login/forgotpassword"}>
+          <Button>Forgot Password</Button>
+        </Link>
+        <Link to={"/dashboard"}>
+          <Button>Dashboard</Button>
+        </Link>
+        <Link to={"/employeedirectory"}>
+          <Button>Employee Directory</Button>
+        </Link>
+        <Link to={"/ess/changepassword"}>
+          <Button>Change Password</Button>
+        </Link>
+        <Link to={"/ess/leaveapplication"}>
+          <Button>Leave Application</Button>
+        </Link>
+        <Link to={"/hr/masterdepartment"}>
+          <Button>Master Department</Button>
+        </Link>
+        <Link to={"/hr/masterposition"}>
+          <Button>Master Position</Button>
+        </Link>
+        <Link to={"/hr/manageevent"}>
+          <Button>Manage Event</Button>
+        </Link>
+        <Link to={"/hr/manageemployee"}>
+          <Button>Manage Employee</Button>
+        </Link>
+        <Link to={"/hr/employeeterminated"}>
+          <Button>Employee Terminated</Button>
+        </Link>
+        <Link to={"/hr/attendancesummary"}>
+          <Button>Attendance Summary</Button>
+        </Link>
+        <Link to={"/hr/leaveapproval"}>
+          <Button>Leave Approval</Button>
+        </Link>
+      </VStack>
+    </>
+  );
+};
+
+export default MasterPosition;
