ba10d61afc213374d0fcd7860937d387e58e5067
Ryan Adiyantara <adiyantararyan@gmail.com>
2024-11-08 21:29:21 +0700

    Fix input form, half complete

--- frontend/src/pages/hr.masterdepartment.jsx
+++ frontend/src/pages/hr.masterdepartment.jsx
@@ -2,7 +2,12 @@ import {
   Box,
   Button,
   Flex,
+  FormControl,
+  FormLabel,
   HStack,
+  Icon,
+  Input,
+  Switch,
   Table,
   Tbody,
   Text,
@@ -14,8 +19,7 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-import { IoMoon } from "react-icons/io5";
-import { LuSun } from "react-icons/lu";
+import { FaApple, FaFacebook, FaGoogle } from "react-icons/fa";
 
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
@@ -27,6 +31,12 @@ const MasterDepartment = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   const textColor = useColorModeValue("gray.700", "white");
   const borderColor = useColorModeValue("gray.200", "gray.600");
+
+  const bgForm = useColorModeValue("white", "navy.800");
+  const titleColor = useColorModeValue("gray.700", "blue.500");
+  const colorIcons = useColorModeValue("gray.700", "white");
+  const bgIcons = useColorModeValue("trasnparent", "navy.700");
+  const bgIconsHover = useColorModeValue("gray.50", "whiteAlpha.100");
 
   return (
     <>
@@ -115,8 +125,165 @@ const MasterDepartment = () => {
               </Box>
             </Box>
           </VStack>
-          <VStack background="bisque" w="260px">
-            <Text>Test Kalau kalau kalau</Text>
+
+          <VStack w="400px">
+            <Flex alignItems="center" justifyContent="center" mb="60px">
+              <Flex
+                direction="column"
+                w="400px"
+                background="transparent"
+                borderRadius="15px"
+                p="40px"
+                bg={bgForm}
+                boxShadow={useColorModeValue("0px 5px 14px rgba(0, 0, 0, 0.05)", "unset")}
+              >
+                <Text
+                  fontSize="xl"
+                  color={textColor}
+                  fontWeight="bold"
+                  textAlign="center"
+                  mb="22px"
+                >
+                  Register With
+                </Text>
+                <HStack spacing="15px" justify="center" mb="22px">
+                  <Flex
+                    justify="center"
+                    align="center"
+                    w="75px"
+                    h="75px"
+                    borderRadius="8px"
+                    border={useColorModeValue("1px solid", "0px")}
+                    borderColor="gray.200"
+                    cursor="pointer"
+                    transition="all .25s ease"
+                    bg={bgIcons}
+                    _hover={{ bg: bgIconsHover }}
+                  >
+                    <Link href="#">
+                      <Icon as={FaFacebook} color={colorIcons} w="30px" h="30px" />
+                    </Link>
+                  </Flex>
+                  <Flex
+                    justify="center"
+                    align="center"
+                    w="75px"
+                    h="75px"
+                    borderRadius="8px"
+                    border={useColorModeValue("1px solid", "0px")}
+                    borderColor="gray.200"
+                    cursor="pointer"
+                    transition="all .25s ease"
+                    bg={bgIcons}
+                    _hover={{ bg: bgIconsHover }}
+                  >
+                    <Link href="#">
+                      <Icon
+                        as={FaApple}
+                        color={colorIcons}
+                        w="30px"
+                        h="30px"
+                        _hover={{ filter: "brightness(120%)" }}
+                      />
+                    </Link>
+                  </Flex>
+                  <Flex
+                    justify="center"
+                    align="center"
+                    w="75px"
+                    h="75px"
+                    borderRadius="8px"
+                    border={useColorModeValue("1px solid", "0px")}
+                    borderColor="gray.200"
+                    cursor="pointer"
+                    transition="all .25s ease"
+                    bg={bgIcons}
+                    _hover={{ bg: bgIconsHover }}
+                  >
+                    <Link href="#">
+                      <Icon
+                        as={FaGoogle}
+                        color={colorIcons}
+                        w="30px"
+                        h="30px"
+                        _hover={{ filter: "brightness(120%)" }}
+                      />
+                    </Link>
+                  </Flex>
+                </HStack>
+                <Text fontSize="lg" color="gray.400" fontWeight="bold" textAlign="center" mb="22px">
+                  or
+                </Text>
+                <FormControl>
+                  <FormLabel ms="4px" fontSize="sm" fontWeight="normal">
+                    Name
+                  </FormLabel>
+                  <Input
+                    variant="auth"
+                    fontSize="sm"
+                    ms="4px"
+                    type="text"
+                    placeholder="Your full name"
+                    mb="24px"
+                    size="lg"
+                  />
+                  <FormLabel ms="4px" fontSize="sm" fontWeight="normal">
+                    Email
+                  </FormLabel>
+                  <Input
+                    variant="auth"
+                    fontSize="sm"
+                    ms="4px"
+                    type="email"
+                    placeholder="Your email address"
+                    mb="24px"
+                    size="lg"
+                  />
+                  <FormLabel ms="4px" fontSize="sm" fontWeight="normal">
+                    Password
+                  </FormLabel>
+                  <Input
+                    variant="auth"
+                    fontSize="sm"
+                    ms="4px"
+                    type="password"
+                    placeholder="Your password"
+                    mb="24px"
+                    size="lg"
+                  />
+                  <FormControl display="flex" alignItems="center" mb="24px">
+                    <Switch id="remember-login" colorScheme="blue" me="10px" />
+                    <FormLabel htmlFor="remember-login" mb="0" fontWeight="normal">
+                      Remember me
+                    </FormLabel>
+                  </FormControl>
+                  <Button
+                    fontSize="10px"
+                    variant="dark"
+                    fontWeight="bold"
+                    w="100%"
+                    h="45"
+                    mb="24px"
+                  >
+                    SIGN UP
+                  </Button>
+                </FormControl>
+                <Flex
+                  flexDirection="column"
+                  justifyContent="center"
+                  alignItems="center"
+                  maxW="100%"
+                  mt="0px"
+                >
+                  <Text color={textColor} fontWeight="medium">
+                    Already have an account?
+                    <Link color={titleColor} as="span" ms="5px" href="#" fontWeight="bold">
+                      Sign In
+                    </Link>
+                  </Text>
+                </Flex>
+              </Flex>
+            </Flex>
           </VStack>
         </HStack>
 
