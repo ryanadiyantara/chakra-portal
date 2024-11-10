689719f74011a3e03da770ae3c58a7d8ff4be59c
Ryan Adiyantara <adiyantararyan@gmail.com>
2024-11-10 14:30:30 +0700

    Can already run create, read and delete, update soon..

--- frontend/src/pages/hr.masterdepartment.jsx
+++ frontend/src/pages/hr.masterdepartment.jsx
@@ -15,23 +15,26 @@ import {
   useToast,
   useColorModeValue,
   VStack,
+  Td,
 } from "@chakra-ui/react";
-import React, { useState } from "react";
+import { FaPen, FaTrash } from "react-icons/fa";
+import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 import { useDepartmentStore } from "../store/department";
+
 // Delete Later..
-import { tablesTableData } from "../components/variables/general";
-import TablesTableRow from "../components/TablesTableRow";
+// import { tablesTableData } from "../components/variables/general";
 
 const MasterDepartment = () => {
   // BE
+  const toast = useToast();
+  const { departments, createDepartment, fetchDepartments, updateDepartment, deleteDepartment } =
+    useDepartmentStore();
   const [newDepartment, setNewDepartment] = useState({
     department_name: "",
   });
-  const toast = useToast();
-  const { createDepartment } = useDepartmentStore();
   const handleAddDepartment = async () => {
     const { success, message } = await createDepartment(newDepartment);
     if (!success) {
@@ -51,11 +54,37 @@ const MasterDepartment = () => {
     }
     setNewDepartment({ department_name: "" });
   };
+  useEffect(() => {
+    fetchDepartments();
+  }, [fetchDepartments]);
+  console.log("departments", departments);
+  const handleDeleteDepartment = async (pid) => {
+    const { success, message } = await deleteDepartment(pid);
+    if (!success) {
+      toast({
+        title: "Error",
+        description: message,
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+    } else {
+      toast({
+        title: "Success",
+        description: message,
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  };
 
   // FE
   const textColor = useColorModeValue("gray.700", "white");
   const borderColor = useColorModeValue("gray.200", "gray.600");
   const bgForm = useColorModeValue("white", "navy.800");
+  const titleColor = useColorModeValue("gray.700", "white");
+  const bgStatus = useColorModeValue("gray.400", "navy.900");
 
   return (
     <>
@@ -103,7 +132,7 @@ const MasterDepartment = () => {
             <Box overflowX={{ sm: "scroll", xl: "hidden" }} pb="0px">
               <Box p="6px 0px 22px 0px">
                 <Text fontSize="xl" color={textColor} fontWeight="bold">
-                  Authors Table
+                  Department List
                 </Text>
               </Box>
               <Box>
@@ -111,36 +140,70 @@ const MasterDepartment = () => {
                   <Thead>
                     <Tr my=".8rem" pl="0px" color="gray.400">
                       <Th pl="0px" borderColor={borderColor} color="gray.400">
-                        Author
+                        Department ID
                       </Th>
                       <Th borderColor={borderColor} color="gray.400">
-                        Function
+                        Department Name
                       </Th>
                       <Th borderColor={borderColor} color="gray.400">
-                        Status
-                      </Th>
-                      <Th borderColor={borderColor} color="gray.400">
-                        Employed
+                        Action
                       </Th>
                       <Th borderColor={borderColor}></Th>
                     </Tr>
                   </Thead>
+
                   <Tbody>
-                    {tablesTableData.map((row, index, arr) => {
-                      return (
-                        <TablesTableRow
-                          name={row.name}
-                          logo={row.logo}
-                          email={row.email}
-                          subdomain={row.subdomain}
-                          domain={row.domain}
-                          status={row.status}
-                          date={row.date}
-                          isLast={index === arr.length - 1 ? true : false}
-                          key={index}
-                        />
-                      );
-                    })}
+                    {departments.map((department) => (
+                      <Tr key={department._id}>
+                        <Td minWidth={{ sm: "250px" }} pl="0px" borderColor={borderColor} py={5}>
+                          <Text fontSize="md" color={titleColor} fontWeight="bold" minWidth="100%">
+                            {department._id}
+                          </Text>
+                        </Td>
+                        <Td borderColor={borderColor}>
+                          <Text fontSize="md" color={titleColor} fontWeight="bold" minWidth="100%">
+                            {department.department_name}
+                          </Text>
+                        </Td>
+
+                        {/* <Td borderColor={borderColor}>
+                        <Flex direction="column">
+                          <Text fontSize="md" color={textColor} fontWeight="bold">
+                            ABC
+                          </Text>
+                          <Text fontSize="sm" color="gray.400" fontWeight="normal">
+                            DEF
+                          </Text>
+                        </Flex>
+                      </Td> */}
+
+                        {/* Action */}
+                        <Td borderColor={borderColor}>
+                          <Flex direction="row" p="0px" alignItems="center" gap="4">
+                            {/* Button for Edit */}
+                            <Flex alignItems="center" gap="1" as="button">
+                              <FaPen size="14" color={textColor} />
+                              <Text fontSize="14px" color={textColor} fontWeight="bold">
+                                EDIT
+                              </Text>
+                            </Flex>
+
+                            {/* Button for Delete */}
+                            <Flex
+                              alignItems="center"
+                              gap="1"
+                              as="button"
+                              onClick={() => handleDeleteDepartment(department._id)}
+                            >
+                              <FaTrash size="14" color="#E53E3E" />
+                              <Text fontSize="14px" color="#E53E3E" fontWeight="bold">
+                                DELETE
+                              </Text>
+                            </Flex>
+                          </Flex>
+                        </Td>
+                      </Tr>
+                    ))}
                   </Tbody>
                 </Table>
               </Box>
