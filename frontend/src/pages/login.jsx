c79c2c81d4d34e4d4b245d6db8df524da03823cf
Ryan Adiyantara <adiyantararyan@gmail.com>
2024-11-28 16:58:51 +0700

    Create login frontend

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,60 +1,166 @@
-import { Button, Text, VStack } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  HStack,
+  Input,
+  Text,
+  useColorModeValue,
+  useToast,
+  VStack,
+} from "@chakra-ui/react";
+import { Link, useNavigate } from "react-router-dom";
+
+import { useUserStore } from "../store/user";
+import { useState } from "react";
 
 const Login = () => {
+  // BE
+  const { users, loginUser } = useUserStore();
+
+  const [newUser, setNewUser] = useState({
+    email: "",
+    user_password: "",
+  });
+
+  const [errors, setErrors] = useState({});
+  const navigate = useNavigate();
+
+  const handleSubmit = async () => {
+    const currentErrors = {};
+
+    if (!newUser.email) currentErrors.email = "Email is required";
+    if (!newUser.user_password) currentErrors.user_password = "Password is required";
+
+    setErrors(currentErrors);
+    if (Object.keys(currentErrors).length > 0) {
+      return; // Stop further execution
+    }
+
+    const { success, message } = await loginUser(newUser);
+
+    if (success) {
+      toast({
+        title: "Success",
+        description: message,
+        status: "success",
+        isClosable: true,
+      });
+      setNewUser({
+        email: "",
+        user_password: "",
+      });
+      navigate("/dashboard");
+    } else {
+      toast({
+        title: "Error",
+        description: message,
+        status: "error",
+        isClosable: true,
+      });
+    }
+  };
+
+  // FE
+  const toast = useToast();
+  const textColor = useColorModeValue("gray.700", "white");
+  const borderColor = useColorModeValue("gray.200", "gray.600");
+  const bgForm = useColorModeValue("white", "navy.800");
   return (
     <>
-      <VStack spacing={2} alignItems={"center"}>
-        <Text
-          fontSize={{ base: "22", sm: "28" }}
-          fontWeight={"bold"}
-          textTransform={"uppercase"}
-          textAlign={"center"}
-          bgGradient={"linear(to-r, cyan.400, blue.500)"}
-          bgClip={"text"}
+      <Box
+        w={{
+          base: "100%",
+          xl: "calc(100% - 275px)",
+        }}
+        float="right"
+        maxWidth="100%"
+        overflow="auto"
+        position="relative"
+        maxHeight="100%"
+        transition="all 0.33s cubic-bezier(0.685, 0.0473, 0.346, 1)"
+        transitionDuration=".2s, .2s, .35s"
+        transitionProperty="top, bottom, width"
+        transitionTimingFunction="linear, linear, ease"
+      >
+        <HStack
+          flexDirection={{
+            base: "column",
+            xl: "row",
+          }}
+          justifyContent="space-between"
+          px={{ base: "30px", xl: "40px" }}
+          py="20px"
+          w="100%"
+          spacing={{ base: "20px", xl: "30px" }}
+          alignItems="start"
+          minHeight="85vh"
         >
-          Login
-        </Text>
-        <Link to={"/login"}>
-          <Button>Login</Button>
-        </Link>
-        <Link to={"/login/forgotpassword"}>
-          <Button>Forgot Password</Button>
-        </Link>
-        <Link to={"/dashboard"}>
-          <Button>Dashboard</Button>
-        </Link>
-        <Link to={"/employeedirectory"}>
-          <Button>Employee Directory</Button>
-        </Link>
-        <Link to={"/ess/changepassword"}>
-          <Button>Change Password</Button>
-        </Link>
-        <Link to={"/ess/leaveapplication"}>
-          <Button>Leave Application</Button>
-        </Link>
-        <Link to={"/hr/masterdepartment"}>
-          <Button>Master Department</Button>
-        </Link>
-        <Link to={"/hr/masterposition"}>
-          <Button>Master Position</Button>
-        </Link>
-        <Link to={"/hr/manageevent"}>
-          <Button>Manage Event</Button>
-        </Link>
-        <Link to={"/hr/manageemployee"}>
-          <Button>Manage Employee</Button>
-        </Link>
-        <Link to={"/hr/employeeterminated"}>
-          <Button>Employee Terminated</Button>
-        </Link>
-        <Link to={"/hr/attendancesummary"}>
-          <Button>Attendance Summary</Button>
-        </Link>
-        <Link to={"/hr/leaveapproval"}>
-          <Button>Leave Approval</Button>
-        </Link>
-      </VStack>
+          {/* Input Form */}
+          <VStack w="400px">
+            <Flex alignItems="center" justifyContent="center" mb="60px">
+              <Flex
+                direction="column"
+                w="400px"
+                background="transparent"
+                borderRadius="15px"
+                p="40px"
+                bg={bgForm}
+              >
+                <Text fontSize="xl" color={textColor} fontWeight="bold" mb="22px">
+                  Login
+                </Text>
+                <FormControl>
+                  <FormLabel ms="4px" fontSize="sm" fontWeight="normal">
+                    Email
+                  </FormLabel>
+                  <Input
+                    fontSize="sm"
+                    ms="4px"
+                    type="text"
+                    mb="24px"
+                    size="lg"
+                    placeholder="Your email"
+                    name="email"
+                    value={newUser.email}
+                    onChange={(e) => setNewUser({ ...newUser, email: e.target.value })}
+                    borderColor={errors.email ? "red.500" : "gray.200"}
+                  />
+                  <FormLabel ms="4px" fontSize="sm" fontWeight="normal">
+                    Password
+                  </FormLabel>
+                  <Input
+                    fontSize="sm"
+                    ms="4px"
+                    type="text"
+                    mb="24px"
+                    size="lg"
+                    placeholder="Your password"
+                    name="user_password"
+                    value={newUser.user_password}
+                    onChange={(e) => setNewUser({ ...newUser, user_password: e.target.value })}
+                    borderColor={errors.user_password ? "red.500" : "gray.200"}
+                  />
+
+                  <Button
+                    fontSize="14px"
+                    variant="dark"
+                    fontWeight="bold"
+                    w="100%"
+                    h="45"
+                    mt="24px"
+                    onClick={handleSubmit}
+                  >
+                    Log In
+                  </Button>
+                </FormControl>
+              </Flex>
+            </Flex>
+          </VStack>
+        </HStack>
+      </Box>
     </>
   );
 };
