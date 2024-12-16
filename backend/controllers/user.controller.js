1a95825de98885177c06c4692f794ca3662041f6
Ryan Adiyantara <adiyantararyan@gmail.com>
2024-12-16 12:00:17 +0700

    Add get current user function on user controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -92,9 +92,32 @@ export const createUsers = async (req, res) => {
 export const getUsers = async (req, res) => {
   try {
     const users = await User.find({})
-      .populate("department_id", "department_name") // Ambil nama department
+      .populate("department_id", "department_name")
       .populate("position_id", "position_name");
     res.status(200).json({ success: true, data: users });
+  } catch (error) {
+    console.log("Error in Fetching users:", error.message);
+    res.status(404).json({ success: false, message: "Server Error" });
+  }
+};
+
+export const getCurrentUsers = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ success: false, message: "Invalid User ID" });
+  }
+
+  try {
+    const user = await User.findById(id)
+      .populate("department_id", "department_name")
+      .populate("position_id", "position_name");
+
+    if (!user) {
+      return res.status(404).json({ success: false, message: "User not found" });
+    }
+
+    res.status(200).json({ success: true, data: user });
   } catch (error) {
     console.log("Error in Fetching users:", error.message);
     res.status(404).json({ success: false, message: "Server Error" });
