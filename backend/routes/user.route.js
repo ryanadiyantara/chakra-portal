fb999ca6a6b017b27a9537397499216819eac1d9
Ryan Adiyantara <adiyantararyan@gmail.com>
2024-12-16 11:59:32 +0700

    Fix data on access token and add get with id on user route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { getUsers, createUsers, updateUsers, deleteUsers } from "../controllers/user.controller.js";
+import {
+  getUsers,
+  getCurrentUsers,
+  createUsers,
+  updateUsers,
+  deleteUsers,
+} from "../controllers/user.controller.js";
 import verifyJWT from "../middleware/verifyJWT.js";
 
 const router = express.Router();
@@ -7,6 +13,7 @@ const router = express.Router();
 router.use(verifyJWT);
 
 router.get("/", getUsers);
+router.get("/:id", getCurrentUsers);
 router.post("/", createUsers);
 router.put("/:id", updateUsers);
 router.delete("/:id", deleteUsers);
